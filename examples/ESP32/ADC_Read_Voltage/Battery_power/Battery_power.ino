d7cb60408f06a902ade087a7d4f6417955497f6c
Nicolas CLERC <nicolas.clerc@sablefin.net>
2024-04-23 11:51:50 +0200

    .

--- examples/ESP32/ADC_Read_Voltage/Battery_power/Battery_power.ino
+++ examples/ESP32/ADC_Read_Voltage/Battery_power/Battery_power.ino
@@ -24,53 +24,65 @@
 #include <Wire.h>
 #include "heltec.h"
 
-//#define Vext 21
-//
-//#define SDA      4  //OLED SDA pin
-//#define SCL     15 //OLED SCL pin
-//#define RST     16 //OLED nRST pin
+#define BATTERY_ADCPIN 13
 #define Fbattery    3700  //The default battery is 3700mv when the battery is fully charged.
 
 float XS = 0.00225;      //The returned reading is multiplied by this XS to get the battery voltage.
 uint16_t MUL = 1000;
 uint16_t MMUL = 100;
-//SSD1306  display(0x3c, SDA, SCL, RST);
+
+double ReadVoltage(byte pin){
+  double reading = analogRead(pin); // Reference voltage is 3v3 so maximum reading is 3v3 = 4095 in range 0 to 4095
+  if(reading < 1 || reading >= 4095)
+    //return 0;
+  // return -0.000000000009824 * pow(reading,3) + 0.000000016557283 * pow(reading,2) + 0.000854596860691 * reading + 0.065440348345433;
+  return -0.000000000000016 * pow(reading,4) + 0.000000000118171 * pow(reading,3)- 0.000000301211691 * pow(reading,2)+ 0.001109019271794 * reading + 0.034143524634089;
+} // Added an improved polynomial, use either, comment out as required
+
 
 void setup()
 {
-//	pinMode(Vext, OUTPUT);
-//	digitalWrite(Vext, LOW);
-//	delay(1000);
-Heltec.begin(true /*DisplayEnable Enable*/, false /*LoRa Enable*/, true /*Serial Enable*/);
+
+  Heltec.begin(true /*DisplayEnable Enable*/, false /*LoRa Enable*/, false /*Serial Enable*/);
 
 	Heltec.display->init();
-	Heltec.display->flipScreenVertically();
-	Heltec.display->setFont(ArialMT_Plain_10);
+	//Heltec.display->flipScreenVertically();
+	//Heltec.display->setFont(ArialMT_Plain_10);
 	Heltec.display->drawString(0, 0, "OLED Start");
 	Heltec.display->display();
 	delay(1000);
 	Heltec.display->clear();
 
-	adcAttachPin(13);
-	analogSetClockDiv(255); // 1338mS
+	//adcAttachPin(BATTERY_ADCPIN);
+	//analogSetClockDiv(255); // 1338mS
 
-	Serial.begin(115200);
+	//Serial.begin(115200);
 }
 
 void loop()
 {
-   uint16_t c  =  analogRead(13)*XS*MUL;
-   //uint16_t d  =  (analogRead(13)*XS*MUL*MMUL)/Fbattery;
-   Serial.println(analogRead(13));
-   //Serial.println((String)d);
-  // Serial.printf("%x",analogRead(13));
+   uint16_t pinBatVal=analogRead(BATTERY_ADCPIN);
+   uint16_t c  =  pinBatVal*XS*MUL;
+   uint16_t d  =  (pinBatVal*XS*MUL*MMUL)/Fbattery;
+   //Serial.println(analogRead(BATTERY_ADCPIN));
+   //   Serial.println((String)d);
+   //   Serial.printf("%x",analogRead(BATTERY_ADCPIN));
    Heltec.display->drawString(0, 0, "Remaining battery still has:");
-   Heltec.display->drawString(0, 10, "VBAT:");
+   Heltec.display->drawString(0, 10, "VBAT: ");
    Heltec.display->drawString(35, 10, (String)c);
    Heltec.display->drawString(60, 10, "(mV)");
-  // Heltec.display->drawString(90, 10, (String)d);
-  // Heltec.display->drawString(98, 10, ".";
-  // Heltec.display->drawString(107, 10, "%");
+
+   Heltec.display->drawString(0, 20, "Capacity: ");
+   Heltec.display->drawString(50, 20, (String)d);
+   //Heltec.display->drawString(98, 20, ".");
+   Heltec.display->drawString(65, 20, "%");
+
+   double voltage=ReadVoltage(13);
+   Heltec.display->drawString(0,35,"Accurant voltage:");
+   Heltec.display->drawString(100,35,(String)voltage);
+
+   Heltec.display->drawString(0,50,"PROUT");
+   
    Heltec.display->display();
    delay(2000);
    Heltec.display->clear();
